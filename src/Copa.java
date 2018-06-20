e648bc360730916b22dfc43ddb355b04685868e9
Felipe Cannarozzo <felipe_cannarozzol@yahoo.com.br>
2018-06-20 18:29:33 -0300

    Classe copa: implementado o método cadastrarJogo()

--- src/Copa.java
+++ src/Copa.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.xml.crypto.Data;
 
 public class Copa {
 
@@ -14,16 +17,18 @@ public class Copa {
 		Copa copa = new Copa();
 		Scanner s = new Scanner(System.in);
 		int menu = 0;
-		
-		while(menu != 10) {
+
+		while (menu != 10) {
 			System.out.println("Sistema copa do mundo. Digite 10 para sair");
 			System.out.println("1- Cadastrar seleções");
 			System.out.println("2- Listar todas seleções");
 			System.out.println("3- Listar seleções por grupo");
-			
+			System.out.println("4- Cadastrar jogo");
+			System.out.println("5- Listar jogos cadastrados por grupo");
+
 			menu = s.nextInt();
-			
-			switch(menu) {
+
+			switch (menu) {
 			case 1:
 				copa.cadastrarSelecoes();
 				break;
@@ -33,14 +38,20 @@ public class Copa {
 			case 3:
 				copa.listarSelecoesGrupo();
 				break;
+			case 4:
+				copa.cadastrarJogo();
+				break;
+			case 5:
+				copa.listarJogosGrupo();
+				break;
 			case 10:
 				System.out.println("Programa encerrado");
 				break;
 			default:
 				System.out.println("Opção inválida");
-				break;			
+				break;
 			}
-			
+
 		}
 
 	}
@@ -94,38 +105,43 @@ public class Copa {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			controle++;
 		}
 		System.out.println();
 
 	}
 
-	public void listarSelecoes(){
-		DAO dao = new DAO("selecao.dat"); // instanciando um objeto do tipo DAO para acesso dos objetos no arquivo selecao.dat 
+	public void listarSelecoes() {
+		DAO dao = new DAO("selecao.dat"); // instanciando um objeto do tipo DAO para acesso dos objetos no arquivo
+											// selecao.dat
 		List selecoes = null; // Criando um list genérico vazio
 		try {
-			selecoes = dao.findAll(); // invocando o método findAll() do objeto dao que retorna uma lista de objetos de forma genérica
-			for(Object selecao : selecoes) { // fazendo um for para percorrer cada objeto existente no List selecoes
-				System.out.println(selecao); // imprimindo na tela cada objeto seleção contido na lista de seleções (método toString imprime os atributos do objeto)
+			selecoes = dao.findAll(); // invocando o método findAll() do objeto dao que retorna uma lista de objetos
+										// de forma genérica
+			for (Object selecao : selecoes) { // fazendo um for para percorrer cada objeto existente no List selecoes
+				System.out.println(selecao); // imprimindo na tela cada objeto seleção contido na lista de seleções
+												// (método toString imprime os atributos do objeto)
 			}
 		} catch (ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		//selecoes.stream().forEach( selecao -> System.out.println(selecao));
+
+		// selecoes.stream().forEach( selecao -> System.out.println(selecao));
 
 	}
 
 	public void listarSelecoesGrupo(char grupo) {
-		DAO<Selecao> dao = new DAO<>("selecao.dat"); // Instanciando um objeto do tipo DAO, porém com tipo forte. É um objeto DAO para o tipo Selecao
+		DAO<Selecao> dao = new DAO<>("selecao.dat"); // Instanciando um objeto do tipo DAO, porém com tipo forte. É um
+														// objeto DAO para o tipo Selecao
 		try {
-			List<Selecao> selecoes = dao.findAll(); // invocando o méotodo findall() que retornará um lista do tipo seleção. Devido a instância parametrizada do objeto DAO.
-			for(Selecao selecao : selecoes) { // Percorrendo a lista de selecoes. 
-				if(selecao.getGrupo() == grupo) { // pergutando se o grupo da seleção iterada é igual ao grupo que usuário passou como parâmetro
-					System.out.println(selecao);  // imprimindo a seleção pertencendo ao grupo passado pelo usuário.
+			List<Selecao> selecoes = dao.findAll(); // invocando o méotodo findall() que retornará um lista do tipo
+													// seleção. Devido a instância parametrizada do objeto DAO.
+			for (Selecao selecao : selecoes) { // Percorrendo a lista de selecoes.
+				if (selecao.getGrupo() == grupo) { // pergutando se o grupo da seleção iterada é igual ao grupo que
+													// usuário passou como parâmetro
+					System.out.println(selecao); // imprimindo a seleção pertencendo ao grupo passado pelo usuário.
 				}
 			}
 		} catch (ClassNotFoundException | IOException e) {
@@ -136,13 +152,23 @@ public class Copa {
 	}
 
 	public void listarSelecoesGrupo() {
-			char grupoChar = selecionarGrupo();
-			listarSelecoesGrupo(grupoChar);
-		
+		char grupoChar = selecionarGrupo();
+		listarSelecoesGrupo(grupoChar);
+
 	}
 
 	public void cadastrarJogo() {
-
+		char gruopo = selecionarGrupo();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite uma data no seguinte formato: dia/mes/ano:");
+		String dataString = s.nextLine();
+		//inverter a data
+		//precisa estar como Ano,mes,dia
+		Date data = new Date(dataString);
+		System.out.println(data);
+		System.out.println("Digite o estádio:");
+		String estadio  = s.next();
+	
 	}
 
 	public void listarJogosGrupo() {
